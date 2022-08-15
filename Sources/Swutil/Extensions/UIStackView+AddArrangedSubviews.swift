637c286d349ec6a7513e15cbc6149429c28d2827
Lucas Fernandez Nicolau <lucasfnicolau@protonmail.com>
2022-08-15 16:32:50 -0300

    feat: Add 'UIStackView+AddArrangedSubviews.swift'

---
+++ Sources/Swutil/Extensions/UIStackView+AddArrangedSubviews.swift
@@ -0,0 +1,14 @@
+//
+//  UIStackView+AddArrangedSubviews.swift
+//  
+//
+//  Created by Lucas Fernandez Nicolau on 15/08/22.
+//
+
+import UIKit
+
+public extension UIStackView {
+    func addArrangedSubviews(_ arrangedSubviews: [UIView]) {
+        arrangedSubviews.forEach { addArrangedSubview($0) }
+    }
+}
